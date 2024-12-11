21137d5fc65c1727729e9f3bb9d856ab3ecdc970
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-12-12 00:48:56 +0530

    Delete Ticket System CLI directory

--- Ticket System CLI/src/TicketInterface.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interface representing a ticket in the ticketing system.
- * This interface defines the basic methods that any ticket class must implement.
- */
-public interface TicketInterface {
-    String getEventName();
-    double getPrice();
-    int getVendorId();
-}
